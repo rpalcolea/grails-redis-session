0642444d1d160367e515d7a6666f222ffc734894
rpalcolea <roberto@perezalcolea.info>
2014-06-02 05:54:07 -0700

    upgrade to 2.3.9

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -2,7 +2,7 @@ grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
 grails.project.target.level = 1.6
-
+grails.project.dependency.resolver = "maven"
 grails.project.dependency.resolution = {
     // inherit Grails' default dependencies
     inherits("global") {
@@ -10,19 +10,23 @@ grails.project.dependency.resolution = {
     log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
     repositories {
         grailsCentral()
+        grailsPlugins()
+        mavenCentral()
     }
     dependencies {
     }
 
     plugins {
-        build(":tomcat:$grailsVersion",
-              ":release:1.0.1") {
+        build(":tomcat:7.0.52.1",
+                ":release:3.0.1",
+                ":rest-client-builder:1.0.3") {
             export = false
+            excludes "svn"
         }
-        compile(":redis:1.5.5", ":database-session:1.2.1", ":webxml:1.4.1")
+        compile(":redis:1.5.5", ":database-session:1.2.1", ":webxml:1.4.1")        {
+            excludes "svn"
+        }
 
-        build ':release:2.2.1', ':rest-client-builder:1.0.3', {
-            export = false
-        }
+
     }
 }
